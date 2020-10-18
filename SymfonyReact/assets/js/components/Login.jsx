e8323ea5302bee93d3536fbcfbadeef5761ed093
TIDALExecution <kurtis.hill1990@gmail.com>
2020-10-18 12:36:29 +0100

    added full login screen

--- SymfonyReact/assets/js/components/Login.jsx
+++ SymfonyReact/assets/js/components/Login.jsx
@@ -53,7 +53,7 @@ function Login(props) {
         <React.Fragment>
             <div className="bg-gradient-primary">
                 <div className="container">
-                    <div className="row justify-content-center">
+                    <div className="row justify-content-center" style={{height:'100vh'}}>
                         <div className="col-xl-5 col-lg-2 col-md-12">
                             <div className="card o-hidden border-0 shadow-lg my-5">
                                 <div className="card-body p-0">
