33e886022243fc5b3ed241a5f48d998bc8ea78f2
TIDALExecution <58777251+TIDALExecution@users.noreply.github.com>
2020-12-04 19:22:47 +0000

    initial creationg of DTO

--- SymfonyReact/src/DTOs/Sensors/CardDataDTO.php
+++ SymfonyReact/src/DTOs/Sensors/CardDataDTO.php
@@ -6,5 +6,26 @@ namespace App\DTOs\Sensors;
 
 class CardDataDTO
 {
+    private $sensorData = [];
 
+    private $sensorName;
+
+    private $sensorType;
+
+    private $sensorRoom;
+
+    public function __construct()
+    {
+
+    }
+
+    private function setSensorData()
+    {
+
+    }
+
+    private function setSensorRoom()
+    {
+
+    }
 }
