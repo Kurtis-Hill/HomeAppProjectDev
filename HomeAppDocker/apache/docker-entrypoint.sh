2bcf0d877db30b90d5c2c294b4de656143cf2943
Kurtis-Hill <kurtis.hill1990@gmail.com>
2021-07-12 19:05:42 +0100

    Docker installation tested and ran smoothly

--- HomeAppDocker/apache/docker-entrypoint.sh
+++ HomeAppDocker/apache/docker-entrypoint.sh
@@ -7,14 +7,15 @@ php -d memory_limit=-1 `which composer` install --prefer-dist --no-interaction
 
 echo "...Composer packages installed"
 
-# echo "Querying test database"
+echo "Querying test database"
 
-# if ! php bin/console dbal:run-sql "select * from user limit 1" --env=test > /dev/null -gt 1; then
-    # echo "No test database found loading fixtures"
+## not working as intended needs fixing ##
+if ! php bin/console dbal:run-sql "select * from user limit 1" --env=test > /dev/null -gt 1; then
+    echo "No test database found loading fixtures"
     php bin/console doctrine:fixtures:load --no-interaction --env=test
-    # echo "...Fixtures loaded"
-# else 
-#     echo "Test database found"
-# fi    
+    echo "...Fixtures loaded"
+else 
+    echo "Test database found"
+fi    
 
 exec /usr/local/bin/docker-php-entrypoint "$@"
