9877d6239ef234023797d3bb2e06f7170a354e61
kurtis <kurtis.hill1990@gmail.com>
2022-02-25 11:07:54 +0000

    added docker override for dev envrioment

--- HomeAppDocker/apache/docker-entrypoint.sh
+++ HomeAppDocker/apache/docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 #dont think i need this anymore
 sed -E '/xdebug\.remote_host=.+/d' /usr/local/etc/php/conf.d/xdebug.ini > /usr/local/etc/php/conf.d/xdebug.ini.tmp && mv /usr/local/etc/php/conf.d/xdebug.ini.tmp /usr/local/etc/php/conf.d/xdebug.ini
 
+sleep 5
+
 if [ "${1#-}" != "$1" ]; then
 	set -- apache2-foreground "$@"
 fi
@@ -24,11 +26,20 @@ if [ ${APP_ENV} == 'prod' ]; then
 		echo "Removing xdebug config"
     	rm -r /usr/local/etc/php/conf.d/xdebug.ini
 	fi
+
+	echo "Executing database migrations production..."
+	bin/console d:m:m --no-interaction
+	echo "...Migrations complete"
 fi
 
 
 if [ ${APP_ENV} == 'dev' ]; then
+	sleep 5
 	echo "dev container build"
+	echo "Executing database migrations dev enviromennt..."
+	bin/console d:m:m --no-interaction --env=test
+	echo "...Migrations complete"
+
 	echo "Querying test database"
 	 if php bin/console dbal:run-sql "select firstName from user where firstName = 'admin' limit 1" --env=test | grep -q 'array(0)'; then
 		echo "Test database empty loading fixtures..."
