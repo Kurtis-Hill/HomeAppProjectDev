34fe72144a2f2e233e8f2aaccc925a13b46b66b1
kurtis <kurtis.hill1990@gmail.com>
2023-09-21 21:15:28 +0100

    refactored display commands to only show relay for relays

--- SymfonyReact/assets/react-app/Sensors/Components/Display/CommandsDisplay.tsx
+++ SymfonyReact/assets/react-app/Sensors/Components/Display/CommandsDisplay.tsx
@@ -5,46 +5,28 @@ import { SensorTypesEnum } from '../../../Enum/SensorTypesEnum';
 import { capitalizeFirstLetter } from '../../../Common/StringFormatter';
 import { switchSensorRequest } from '../../Request/Sensor/SwitchSensorRequest';
 import DotCircleSpinner from '../../../Common/Components/Spinners/DotCircleSpinner';
+import { RelayUpdateRequestCommand } from '../Commands/RelayUpdateRequestCommand';
 
 export function CommandsDisplay(props: { sensor: SensorResponseInterface }) {
     const { sensor } = props;
     
-    const [relayState, setRelayState] = useState<boolean>(sensor.sensorReadingTypes.relay.currentReading);
-
-    console.log('before click', relayState);
-    const handleRelayUpdate = async (e: Event) => {
-        const target = e.target as HTMLInputElement;
-        const value = target.checked as boolean;
-
-        setRelayState((currentState: boolean) => value)            
-        
-        console.log('current state', relayState);
-        const switchSensorResponse = await switchSensorRequest({
-            'sensorData': [
-                {
-                    'sensorName': sensor.sensorName,
-                    'currentReadings': 
-                        {
-                            'relay': value,
-                        }
-                        
-                }
-            ]
-        })
-        
-        if (switchSensorResponse.status !== 202) {
-            setRelayState((currentState: boolean) => !value)
-        }
-    }
-
-    if (sensor.sensorType.sensorTypeName === SensorTypesEnum.GenericRelay) {
-        const shouldBeChecked = relayState;;
+    // if (sensor.sensorType.sensorTypeName === SensorTypesEnum.GenericRelay) {
+        // const shouldBeChecked = relayState;;
         // const disabled = sensor.sensorReadingTypes.relay.currentReading === true !== sensor.sensorReadingTypes.relay.requestedReading;
-        const disabled = false;
+        // const disabled = false;
 
         return (
-            <>                       
-                <h5 className="title">Send Relay Update Request</h5>
+            <>       
+                {
+                    sensor.sensorType.sensorTypeName === SensorTypesEnum.GenericRelay
+                        ?
+                            <RelayUpdateRequestCommand
+                                sensor={sensor}
+                            />      
+                        :
+                            null
+                }
+                {/* <h5 className="title">Send Relay Update Request</h5>
                 <label className="switch">
                     { disabled ? <DotCircleSpinner classes="center-spinner bool-sensor-button-loading"  /> : null }
                     <input 
@@ -54,10 +36,10 @@ export function CommandsDisplay(props: { sensor: SensorResponseInterface }) {
                         disabled={disabled}
                     />
                     <span className="slider round"></span>
-                </label> 
+                </label>  */}
             </>
         )
-    }
+    
     
     return (
         <>
--- SymfonyReact/src/ORM/Migrations/Version20230920215910.php
+++ SymfonyReact/src/ORM/Migrations/Version20230920215910.php
@@ -7,9 +7,6 @@ namespace App\ORM\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230920215910 extends AbstractMigration
 {
     public function getDescription(): string
@@ -26,20 +23,23 @@ final class Version20230920215910 extends AbstractMigration
             (28, 'couch', 'couch/sofa'),
             (29, 'sun', 'the sun'),
             (30, 'frog', 'frog from the side'),
-            (31, 'water', 'water 3 lines');
+            (31, 'water', 'water 3 lines'),
             (32, 'temperature-low', 'thermometer on low'),
-            (32, 'temperature-high', 'thermometer on high'),
-            (32, 'house-user', 'house with user icon'),
-            (32, 'shower', 'shower head'),
-            (32, 'fan', 'fan blades'),
-            (32, 'lightbulb', 'light bulb'),
+            (33, 'temperature-high', 'thermometer on high'),
+            (34, 'house-user', 'house with user icon'),
+            (35, 'shower', 'shower head'),
+            (36, 'fan', 'fan blades'),
+            (37, 'lightbulb', 'light bulb');
         ");
 
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql("
+            DELETE FROM `icons` 
+                WHERE `iconID` IN (28, 29, 30, 31, 32, 33, 34, 35, 36, 37);
+        ");
 
     }
 }
